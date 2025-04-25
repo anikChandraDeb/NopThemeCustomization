d9eb82864b288b3193dfaacca562bb235eb7d80a
Anik Chandra Deb <anik.deb161@gmail.com>
2025-04-25 12:20:30 +0600

    Updated: SupplierListModel

--- Plugins/Nop.Plugin.Misc.Supplier/Areas/Admin/Model/SupplierListModel.cs
+++ Plugins/Nop.Plugin.Misc.Supplier/Areas/Admin/Model/SupplierListModel.cs
@@ -1,4 +1,4 @@
-﻿using Nop.Web.Framework.Models;
+﻿using Nop.Web.Framework.Models; 
 
 namespace Nop.Plugin.Misc.Supplier.Areas.Admin.Model;
 public record SupplierListModel : BasePagedListModel<SupplierModel>
